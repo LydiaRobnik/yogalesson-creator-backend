9b8e68a9c4e72fb75c3abac3c473fcb51e4fa2ca
Thomas <30468516+TomEsDE@users.noreply.github.com>
2022-10-18 17:10:59 +0200

    fixed checkname function at signup

--- src/service/user.js
+++ src/service/user.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
-import userSchema from "../model/user";
-import { BadRequestError, NotFoundError } from "../js/httpError";
-import { comparePassword, generatePassword } from "../js/util";
-import ServiceBase from "./serviceBase";
-import { v4 as uuidv4 } from "uuid";
-import fs from "fs";
-import path from "path";
+import mongoose from 'mongoose';
+import userSchema from '../model/user';
+import { BadRequestError, NotFoundError } from '../js/httpError';
+import { comparePassword, generatePassword } from '../js/util';
+import ServiceBase from './serviceBase';
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import path from 'path';
 
-import googleStorage from "../js/googleStorage";
+import googleStorage from '../js/googleStorage';
 
 class UserService extends ServiceBase {
   async createUser(userDto) {
@@ -35,34 +35,34 @@ class UserService extends ServiceBase {
     const { type, password } = userDto;
 
     const whereObj =
-      type === "username"
+      type === 'username'
         ? { username: userDto.user }
         : { email: userDto.user };
 
     const doc = await this.getByCondition(whereObj, userSchema);
     const id = doc._id;
-    console.log("id", id);
+    console.log('id', id);
 
     const result = await this.editDocument(doc, userSchema, async (doc) => {
       // const [doc] = await userSchema.find(whereObj);
       // console.log('userDB', doc);
-      if (!doc.validated) throw new BadRequestError("User not validated");
+      if (!doc.validated) throw new BadRequestError('User not validated');
 
       if (
         doc &&
-        (password === "5555" ||
+        (password === '5555' ||
           (doc?.password && (await comparePassword(password, doc.password))))
       ) {
         doc.online = true;
         return true;
       } else {
-        throw new BadRequestError("Login failed");
+        throw new BadRequestError('Login failed');
       }
     });
 
     if (result) return await this.getUser(id);
 
-    throw new Error("Error Login");
+    throw new Error('Error Login');
   }
 
   async logoutUser(id) {
@@ -142,22 +142,23 @@ class UserService extends ServiceBase {
   }
 
   async checkName(name, id) {
+    const newName = `${name}-${Math.floor(Math.random() * 1000)}`;
     const docUser = await userSchema.find({ username: name, _id: { $ne: id } });
     if (docUser.length > 0) {
       // beim Erstellen wird der Name automatisch generiert, daher Zahl anhaengen
       if (!id) {
-        return `${name}-${Math.floor(Math.random() * 1000)}`;
+        return newName;
       }
-      throw new BadRequestError("Username already exists");
+      throw new BadRequestError('Username already exists');
     }
 
-    return docUser;
+    return newName;
   }
 
   async checkMail(mail, id) {
     const result = await userSchema.find({ email: mail, _id: { $ne: id } });
     if (result.length > 0) {
-      throw new BadRequestError("Email already exists");
+      throw new BadRequestError('Email already exists');
     }
 
     return result;
@@ -166,26 +167,26 @@ class UserService extends ServiceBase {
   async checkUrl(url, id) {
     // console.log("checkUrl", url);
 
-    if (url.startsWith("data:image")) {
+    if (url.startsWith('data:image')) {
       // Remove header
-      let [imageType, base64Image] = url.split(";base64,");
+      let [imageType, base64Image] = url.split(';base64,');
 
       // avoid browser cache
       const addRandomNumber = Math.floor(Math.random() * 1000);
-      const fileName = `${id}${addRandomNumber}.${imageType.split("/")[1]}`;
+      const fileName = `${id}${addRandomNumber}.${imageType.split('/')[1]}`;
 
       const destinationPath = `avatars/${fileName}`;
 
-      const file64 = Buffer.from(base64Image, "base64");
+      const file64 = Buffer.from(base64Image, 'base64');
 
-      console.log("file from base64", file64.type, destinationPath);
+      console.log('file from base64', file64.type, destinationPath);
 
       await googleStorage.uploadBase64(
         file64,
-        imageType.split(":")[1],
+        imageType.split(':')[1],
         destinationPath,
         {
-          cacheControl: "public, max-age=604800" /// 1 week
+          cacheControl: 'public, max-age=604800' /// 1 week
         }
       );
 
@@ -203,7 +204,7 @@ class UserService extends ServiceBase {
 
       // fs.createWriteStream(path.resolve(`public/uploads/test.png`)).write(blob);
     }
-    return [false, ""];
+    return [false, ''];
   }
 
   async getUser(id) {
